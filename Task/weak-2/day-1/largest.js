10ae926892087caf02f55efbb669331de4245635
HISHAL24 <hishal@madeit.ac.in>
2026-01-12 10:28:07 +0530

    Initial commit

---
+++ Task/weak-2/day-1/largest.js
@@ -0,0 +1,19 @@
+let a = 10;
+let b = 25;
+let c = 15;
+
+if (a > b && a > c) {
+    console.log("Largest is: " + a);
+} else if (b > a && b > c) {
+    console.log("Largest is: " + b);
+} else {
+    console.log("Largest is: " + c);
+}
+
+
+let num = 5;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(num + " x " + i + " = " + (num * i));
+}
+
